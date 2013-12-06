ad791fd4b9c14b548e340ae0f3d1a0dab013bc10
Dan Lynn <git@danlynn.org>
2013-12-05 19:58:34 -0500

    Switched back to normal dig speed.

--- js/maze.js
+++ js/maze.js
@@ -2,9 +2,9 @@ define(['lodash', 'maze_digger'], function(_, MazeDigger) {
 
   /**
    * Construct a new Maze with the specified 'width', 'height', and 'exit'
-   * position.  The maze 'grid' is pre-allocated during construction.  The 
+   * position.  The maze 'grid' is pre-allocated during construction.  The
    * 'exit' point is 'dug' after grid allocation.
-   * 
+   *
    * @param width
    * @param height
    * @param exit {{x: (number), y: (number)}}
@@ -19,7 +19,7 @@ define(['lodash', 'maze_digger'], function(_, MazeDigger) {
     this.context = this.canvas.getContext('2d');
     this.erase();
   };
-  
+
   Maze.prototype = {
 
     directions: [
@@ -28,7 +28,7 @@ define(['lodash', 'maze_digger'], function(_, MazeDigger) {
       {x: 0 , y: 2},
       {x:-2 , y: 0}
     ],
-    
+
     /**
      * Allocated a new 'grid' (erasing any previous grid) and digs the 'exit'
      * point.
@@ -48,25 +48,25 @@ define(['lodash', 'maze_digger'], function(_, MazeDigger) {
     /**
      * Pick a new location in this Maze instance.  The new position must
      * be on an existing corridor and have at least one available direction to
-     * dig.  This method returns the x,y position that was found.  The 
-     * basic algorithm is to pick a random event x,y position and if that 
-     * position is not on an existing tunnel OR valid_location() callback 
-     * returns false then the x,y position is moved in a pre-selected direction 
-     * and this new position is then checked.  The pre-selected direction is 
-     * either north or east.  When the position increments past the top or right 
-     * of the maze then it continues at the bottom or left stepping one row or 
-     * column over.  In this way, it will pick a random starting position to 
-     * start looking and then scan sequentially to find a viable place to 
-     * return.  If the sequential scan finally returns back to the original 
-     * random starting position then the maze has UNSUCCESSFULLY been fully 
-     * scanned for a new valid location and failed - thus, return null.  Upon 
+     * dig.  This method returns the x,y position that was found.  The
+     * basic algorithm is to pick a random event x,y position and if that
+     * position is not on an existing tunnel OR valid_location() callback
+     * returns false then the x,y position is moved in a pre-selected direction
+     * and this new position is then checked.  The pre-selected direction is
+     * either north or east.  When the position increments past the top or right
+     * of the maze then it continues at the bottom or left stepping one row or
+     * column over.  In this way, it will pick a random starting position to
+     * start looking and then scan sequentially to find a viable place to
+     * return.  If the sequential scan finally returns back to the original
+     * random starting position then the maze has UNSUCCESSFULLY been fully
+     * scanned for a new valid location and failed - thus, return null.  Upon
      * finding the first valid location, the x,y location is returned.
-     * 
+     *
      * @callback valid_location passed {x: (number), y: (number)} and returns
      *              true if location is valid - returning false will cause a new
      *              location to be selected and passed to 'valid_location' until
      *              it returns true
-     * @returns {{x: (number), y: (number)}} true if new position found - else 
+     * @returns {{x: (number), y: (number)}} true if new position found - else
      *              false if no open positions left in the maze
      */
     pick_new_location: function(valid_location) {
@@ -97,20 +97,20 @@ define(['lodash', 'maze_digger'], function(_, MazeDigger) {
     },
 
     /**
-     * Generate the maze by ensuring a starting point (exit) is 'dug' and 
+     * Generate the maze by ensuring a starting point (exit) is 'dug' and
      * instantiating a MazeDigger on that point then invoking it.
-     * 
+     *
      * @param width {number} must be even
      * @param height {number} must be even
      */
     dig: function() {
       var maze_digger = new MazeDigger(this.exit.x, this.exit.y, {x: -2.0, y: 0.0}, this);
-      maze_digger.run(200);
+      maze_digger.run(5);
       return maze_digger;
     }
 
   };
-  
+
   return Maze;
-  
+
 });
